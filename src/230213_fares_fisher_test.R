c481760aca3265ab771f87fc69ec3cc24f0865bd
JenFisher7 <jfisher7@uab.edu>
2023-03-30 11:16:27 -0500

    fisher_style

--- src/230213_fares_fisher_test.R
+++ src/230213_fares_fisher_test.R
@@ -17,17 +17,17 @@ library(stringr)
 library(dplyr)
 
 # load in the data 
-patient_safety<- readRDS( paste0(dir_path, "data/patient_safety_filtered_pt.rds"))
-drug_ae_df2<- readRDS(paste0(dir_path, "data/drug_ae_df.rds"))
+patient_safety <- readRDS( paste0(dir_path, "data/patient_safety_filtered_pt.rds"))
+drug_ae_df2 <- readRDS(paste0(dir_path, "data/drug_ae_df.rds"))
 
 #formatting data for function
 
-patient_safety_males <- patient_safety[patient_safety$gender == 1,]
-patient_safety_females <- patient_safety[patient_safety$gender == 2,]
+patient_safety_males <- patient_safety[patient_safety$gender == 1, ]
+patient_safety_females <- patient_safety[patient_safety$gender == 2, ]
 
-additional_col <- matrix(nrow= nrow(drug_ae_df2), ncol=8)
+additional_col <- matrix(nrow = nrow(drug_ae_df2), ncol=8)
 
-drug_ae_df<- cbind(drug_ae_df2, additional_col)
+drug_ae_df <- cbind(drug_ae_df2, additional_col)
 
 #get arguements from the slurm job submission
 args <- commandArgs(trailingOnly = TRUE)
@@ -41,14 +41,14 @@ print(num1)
 N <- nrow(drug_ae_df)
 sz <- 3000 #small enough for the short queue
 sample_list <- split(1:N, ceiling(seq_along(1:N)/sz))
-sample_vector<- sample_list[[num1]] 
+sample_vector <- sample_list[[num1]] 
 
 print("samples")
 print(sample_vector)
 
 #sex_bias_adverse_event_test
 print(date())
-parallel_tests<- mclapply(sample_vector,sex_bias_adverse_event_test, drug_ae_df, mc.cores= 10)
+parallel_tests <- mclapply(sample_vector, sex_bias_adverse_event_test, drug_ae_df, mc.cores= 10)
 print(date())
 
 
